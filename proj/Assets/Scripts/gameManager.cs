ae1378de524c8838a538ddad8cdbc7d66b175d6e
jaisrael <jason.a.israel@gmail.com>
2012-11-28 06:09:16 -0500

    Basic Turret Created
    class is mostly empty :(

--- proj/Assets/Scripts/gameManager.cs
+++ proj/Assets/Scripts/gameManager.cs
@@ -6,13 +6,21 @@ public class gameManager : MonoBehaviour {
 	public gridCreator gc;
 	private GameObject[,] grid;
 
+	private int frame;
+	private int fps;
+
 	// Use this for initialization
 	void Start () {
 		grid = gc.createGameGrid();
+
+		frame = 0;
+		fps = 60;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		frame++;
+
 	}
 }
---
+++ proj/Assets/Scripts/object scripts/turret.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class turret : MonoBehaviour {
+
+	//instantiated through prefab
+	public int identifier; 			//turret type
+	public GameObject missile;		//missile
+	public int rate;				//fireRate
+
+	//instantiated upon creation (gameManager.cs)
+	public gameManager gm;			//gets other object and game data
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
