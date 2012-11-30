3b4c9506e120692a627b69a429cb0f62535e17ec
jaisrael <jason.a.israel@gmail.com>
2012-11-30 08:41:37 -0500

    Game can now exit to main menu
    Clicking the exit button pauses the game, and returns to the previous
    game state if you press no

--- proj/Assets/Scripts/manager subscripts/guiManager.cs
+++ proj/Assets/Scripts/manager subscripts/guiManager.cs
@@ -10,6 +10,7 @@ public class guiManager : MonoBehaviour {
 	//textures
 	public Texture playButtonTexture;
 	public Texture pauseButtonTexture;
+	public Texture exitButtonTexture;
 
 	//creep images
 	public Texture basicTurretImage;
@@ -22,20 +23,32 @@ public class guiManager : MonoBehaviour {
 
 	//size variables
 	private int buttonSize;
+	private int buttonOffset;
 
 	private int waveViewWidth;
 	private int waveViewHeight;
 	private int waveViewVertOffset;
 	private int waveContentOffset;
 
+	private int exitWindowWidth;
+	private int exitWindowHeight;
+	private bool renderExitWindow;
+	private gameState prevState;
+
 	// Use this for initialization
 	void Start () {
 		buttonSize = 64;
+		buttonOffset = 10;
 
 		waveViewWidth = 400;
 		waveViewHeight = 150;
 		waveViewVertOffset = 10;
 		waveContentOffset = 100;
+
+		exitWindowWidth = 200;
+		exitWindowHeight = 100;
+		renderExitWindow = false;
+		prevState = (gameState)0;
 	}
 	
 	// Update is called once per frame
@@ -58,14 +71,14 @@ public class guiManager : MonoBehaviour {
 		GUI.skin = buttonSkin;
 		if(gm.state == gameState.buildPhase || gm.state == gameState.paused)
 		{		
-			if(GUI.Button(new Rect(10,10,buttonSize,buttonSize),playButtonTexture))
+			if(GUI.Button(new Rect(buttonOffset,buttonOffset,buttonSize,buttonSize),playButtonTexture))
 			{
 				gm.state = gameState.battlePhase;
 			}
 		}
 		else if(gm.state == gameState.battlePhase)
 		{
-			if(GUI.Button(new Rect(10,10,buttonSize,buttonSize),pauseButtonTexture))
+			if(GUI.Button(new Rect(buttonOffset,buttonOffset,buttonSize,buttonSize),pauseButtonTexture))
 			{
 				gm.state = gameState.paused;
 			}
@@ -88,10 +101,36 @@ public class guiManager : MonoBehaviour {
 			if(sm.currentWaveIndex+2 < sm.allWaves[sm.currentWave].waveSize) 
 				GUI.Label(new Rect(240,0,50,50),basicCreepImage);
 			
-			
 			GUI.EndScrollView();
 		}
 
 		//Top-Right Button
+		//pulls up game exit window
+		GUI.skin = buttonSkin;
+		if(GUI.Button(new Rect(Screen.width-buttonSize-buttonOffset,buttonOffset,buttonSize,buttonSize),exitButtonTexture))
+		{
+			renderExitWindow = true;
+			prevState = gm.state;
+			gm.state = gameState.paused;
+		}
+		GUI.skin = null;
+		if(renderExitWindow)
+		{
+			Rect exitWindowRect = new Rect((Screen.width-exitWindowWidth)/2,(Screen.height-exitWindowHeight)/2,exitWindowWidth,exitWindowHeight);
+			exitWindowRect = GUI.Window(0, exitWindowRect,exitWindow,"Are You Sure?");
+		}
+	}
+
+	void exitWindow(int windowID)
+	{
+		if (GUI.Button(new Rect(80,25,40,20),"Yes"))
+		{
+			Application.LoadLevel("StartMenu");
+		}
+		if (GUI.Button(new Rect(80,65,40,20),"No"))
+		{
+			renderExitWindow = false;
+			gm.state = prevState;
+		}
 	}
 }
