5fc44e69609a2da137462e683be653d9db0114fe
jaisrael <jason.a.israel@gmail.com>
2012-11-30 09:43:21 -0500

    Basic Touch Works Better
    Reimplemented in a better way

--- proj/Assets/Scripts/manager subscripts/touchManager.cs
+++ proj/Assets/Scripts/manager subscripts/touchManager.cs
@@ -9,11 +9,10 @@ public enum selectedState
 	creep
 }
 
-
-
 public class touchManager : MonoBehaviour {
 
 	public selectedState selected;
+	public GameObject selectedObject;
 
 	// Use this for initialization
 	void Start () {
@@ -32,19 +31,20 @@ public class touchManager : MonoBehaviour {
 			{
 				//when Physics.Raycast succeeds, it fill out the hitInfo object
 				//get the GameObject that the collider belongs to
-				GameObject g = hitInfo.collider.gameObject;
+				selectedObject = hitInfo.collider.gameObject;
 				//print out the GameObject's name
+				//Debug.Log(selectedObject.transform.parent.gameObject.tag);
 
-				if(g.name.Contains("tile")) selected = selectedState.tile;
-				if(g.name.Contains("turret")) selected = selectedState.turret;
-				if(g.name.Contains("creep")) selected = selectedState.creep;
+				if(selectedObject.transform.parent.gameObject.tag == "Tile") selected = selectedState.tile;
+				if(selectedObject.transform.parent.gameObject.tag == "Turret") selected = selectedState.turret;
+				if(selectedObject.transform.parent.gameObject.tag == "Creep") selected = selectedState.creep;
 			}
 			else 
 			{
 				selected = selectedState.none;
 			}
-			Debug.Log(selected);
+			
 		}
-
+		//Debug.Log(selected);
 	}
 }
